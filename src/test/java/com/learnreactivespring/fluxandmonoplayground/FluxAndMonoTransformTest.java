f6220a8d49a8d7fb778dfc605f3ee57c1061adde
Juan Carlos Peña Peña <jucpen@bancolombia.com.co>
2021-09-28 19:16:30 -0500

    Transforming a Reactive Stream using flatMap and using parallel pipelines

--- src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTransformTest.java
+++ src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTransformTest.java
@@ -7,6 +7,8 @@ import reactor.test.StepVerifier;
 import java.util.Arrays;
 import java.util.List;
 
+import static reactor.core.scheduler.Schedulers.parallel;
+
 public class FluxAndMonoTransformTest {
 
     List<String> names = Arrays.asList("adam", "anna", "jack", "jenny");
@@ -59,6 +61,67 @@ public class FluxAndMonoTransformTest {
         StepVerifier.create(namesFlux)
                 .expectNext("JENNY")
                 .verifyComplete();
+    }
+
+    @Test
+    public void transformUsingFlatMap() {
+
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F")) // A, B, C, D, E, F
+            .flatMap(s -> {
+                return Flux.fromIterable(convertToList(s)); // A -> List[A, newValue], B -> List[B, newValue]
+            }) //db or external service call that returns a flux -> s -> Flux<String>
+            .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+
+    }
+
+    private List<String> convertToList(String s) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Arrays.asList(s, "newValue");
+    }
+
+    @Test
+    public void transformUsingFlatMap_usingparallel() {
+
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F")) // A, B, C, D, E, F
+                .window(2)
+                .flatMap(s ->
+                        s.map(this::convertToList)
+                        .subscribeOn(parallel())) // Flux<List<String>>
+                .flatMap(s -> Flux.fromIterable(s)) //Flux<String>
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+
+    }
+
+    @Test
+    public void transformUsingFlatMap_parallel_maintain_order() {
+
+        Flux<String> stringFlux = Flux.fromIterable(Arrays.asList("A", "B", "C", "D", "E", "F")) // A, B, C, D, E, F
+                .window(2)
+                /*.concatMap(s ->
+                        s.map(this::convertToList).subscribeOn(parallel()))*/ // Flux<List<String>>
+                .flatMapSequential(s ->
+                        s.map(this::convertToList)
+                        .subscribeOn(parallel())) //Flux<String>
+                .flatMap(s ->
+                        Flux.fromIterable(s)) //Flux<List<String>>
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(12)
+                .verifyComplete();
+
     }
 
 }
